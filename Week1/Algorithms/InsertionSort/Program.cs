a568b2248e9967c7967b91a4136780caf36f6cb6
Salma <skummesalma463@gmail.com>
2026-07-06 21:58:43 +0530

    changes

---
+++ Week1/Algorithms/InsertionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] numbers = { 12, 11, 13, 5, 6 };
+
+Console.WriteLine("Before Sorting:");
+
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    int key = numbers[i];
+    int j = i - 1;
+
+    while (j >= 0 && numbers[j] > key)
+    {
+        numbers[j + 1] = numbers[j];
+        j--;
+    }
+
+    numbers[j + 1] = key;
+}
+
+Console.WriteLine("\n\nAfter Insertion Sort:");
+
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
