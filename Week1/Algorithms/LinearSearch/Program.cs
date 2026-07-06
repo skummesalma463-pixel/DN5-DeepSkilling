c064bac7f2847ce5b03fe099d6e20673e4794f51
Salma <skummesalma463@gmail.com>
2026-07-06 22:00:27 +0530

    changes

---
+++ Week1/Algorithms/LinearSearch/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = { 10, 20, 30, 40, 50 };
+
+Console.Write("Enter number to search: ");
+int target = Convert.ToInt32(Console.ReadLine());
+
+bool found = false;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] == target)
+    {
+        Console.WriteLine($"Found at index {i}");
+        found = true;
+        break;
+    }
+}
+
+if (!found)
+{
+    Console.WriteLine("Number not found");
+}
