c9bc90e71ebcb866943f0fbcb4e9ff67c66c29a0
Salma <skummesalma463@gmail.com>
2026-07-06 21:53:28 +0530

    changes

---
+++ Week1/Algorithms/BubbleSort/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = { 50, 20, 40, 10, 30 };
+
+Console.WriteLine("Before Sorting:");
+
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
+
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    for (int j = 0; j < numbers.Length - i - 1; j++)
+    {
+        if (numbers[j] > numbers[j + 1])
+        {
+            int temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+    }
+}
+
+Console.WriteLine("\n\nAfter Bubble Sort:");
+
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
